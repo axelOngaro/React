d1410d6f9730caadfc3e3c111fa5d9d21a38a96f
Axel <axel.ongaro.pro@gmail.com>
2021-01-21 16:30:23 +0100

    Added state, added styling, added player, added play/pause

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -1,12 +1,21 @@
-import './styles/app.css';
-import Player from './components/Player';
-import Song from './components/Song';
+import "./styles/app.css";
+import Player from "./components/Player";
+import Song from "./components/Song";
+import data from "./data";
+import React, { useState } from "react";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
